82e77236efcfb88f279829ac843bb38bdd774b82
weuizx <weuizx@gmail.com>
2025-06-15 01:38:19 +0300

    execution-and-statistic
    Добавил обновление сущностей при экзекуции

--- src/main/java/org/evilincorporated/pineapple/service/TestCaseExecutionService.java
+++ src/main/java/org/evilincorporated/pineapple/service/TestCaseExecutionService.java
@@ -3,10 +3,12 @@ package org.evilincorporated.pineapple.service;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.evilincorporated.pineapple.controller.dto.TestCaseExecutionDto;
+import org.evilincorporated.pineapple.domain.entity.TestCase;
 import org.evilincorporated.pineapple.domain.entity.TestCaseExecution;
 import org.evilincorporated.pineapple.domain.entity.User;
 import org.evilincorporated.pineapple.domain.mapper.TestCaseExecutionMapper;
 import org.evilincorporated.pineapple.domain.repository.TestCaseExecutionRepository;
+import org.evilincorporated.pineapple.domain.repository.TestCaseRepository;
 import org.evilincorporated.pineapple.security.service.SecurityUtils;
 import org.springframework.stereotype.Service;
 
@@ -17,15 +19,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TestCaseExecutionService {
 
-    private static final String PROJECT_NOT_FOUND_MESSAGE = "TestCaseExecution not found by id = %s";
+    private static final String TEST_CASE_EXECUTION_NOT_FOUND_MESSAGE = "TestCaseExecution not found by id = %s";
+    private static final String TEST_CASE_NOT_FOUND_MESSAGE = "TestCase not found by id = %s";
 
     private final TestCaseExecutionRepository testCaseExecutionRepository;
+    private final TestCaseRepository testCaseRepository;
     private final TestCaseExecutionMapper testCaseExecutionMapper;
     private final SecurityUtils securityUtils;
 
     public TestCaseExecutionDto createTestCaseExecution(TestCaseExecutionDto testCaseExecutionDto) {
-        //TODO проверять на существование тест кейс
+        //TODO причесать
         User currentUser = securityUtils.getCurrentUser();
+        TestCase testCase = testCaseRepository.findById(testCaseExecutionDto.getTestCaseId()).orElseThrow(
+                () -> new EntityNotFoundException(TEST_CASE_NOT_FOUND_MESSAGE.formatted(testCaseExecutionDto.getTestCaseId())));
+        testCase.setState(testCaseExecutionDto.getState());
+        testCaseRepository.save(testCase);
         testCaseExecutionDto.setUserId(currentUser.getId());
         testCaseExecutionDto.setExecutionDateTime(ZonedDateTime.now());
         TestCaseExecution testCaseExecution = testCaseExecutionRepository.save(testCaseExecutionMapper.testCaseExecutionDtoToTestCaseExecution(testCaseExecutionDto));
@@ -38,14 +46,15 @@ public class TestCaseExecutionService {
 
     public TestCaseExecutionDto getTestCaseExecution(Long id) {
         TestCaseExecution testCaseExecution = testCaseExecutionRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException(PROJECT_NOT_FOUND_MESSAGE.formatted(id)));
+                () -> new EntityNotFoundException(TEST_CASE_EXECUTION_NOT_FOUND_MESSAGE.formatted(id)));
         return testCaseExecutionMapper.testCaseExecutionToTestCaseExecutionDto(testCaseExecution);
     }
 
     public void deleteTestCaseExecution(Long id) {
         TestCaseExecution testCaseExecution = testCaseExecutionRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException(PROJECT_NOT_FOUND_MESSAGE.formatted(id)));
+                () -> new EntityNotFoundException(TEST_CASE_EXECUTION_NOT_FOUND_MESSAGE.formatted(id)));
         testCaseExecutionRepository.delete(testCaseExecution);
     }
+
 }
 
--- src/main/java/org/evilincorporated/pineapple/service/TestCycleExecutionService.java
+++ src/main/java/org/evilincorporated/pineapple/service/TestCycleExecutionService.java
@@ -3,10 +3,13 @@ package org.evilincorporated.pineapple.service;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.evilincorporated.pineapple.controller.dto.TestCycleExecutionDto;
+import org.evilincorporated.pineapple.domain.entity.TestCycle;
 import org.evilincorporated.pineapple.domain.entity.TestCycleExecution;
 import org.evilincorporated.pineapple.domain.entity.User;
+import org.evilincorporated.pineapple.domain.enums.TestCycleState;
 import org.evilincorporated.pineapple.domain.mapper.TestCycleExecutionMapper;
 import org.evilincorporated.pineapple.domain.repository.TestCycleExecutionRepository;
+import org.evilincorporated.pineapple.domain.repository.TestCycleRepository;
 import org.evilincorporated.pineapple.security.service.SecurityUtils;
 import org.springframework.stereotype.Service;
 
@@ -17,15 +20,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TestCycleExecutionService {
 
-    private static final String PROJECT_NOT_FOUND_MESSAGE = "TestCycleExecution not found by id = %s";
+    private static final String TEST_CYCLE_EXECUTION_NOT_FOUND_MESSAGE = "TestCycleExecution not found by id = %s";
+    private static final String TEST_CYCLE_NOT_FOUND_MESSAGE = "TestCycle not found by id = %s";
 
     private final TestCycleExecutionRepository testCycleExecutionRepository;
+    private final TestCycleRepository testCycleRepository;
     private final TestCycleExecutionMapper testCycleExecutionMapper;
     private final SecurityUtils securityUtils;
 
     public TestCycleExecutionDto createTestCycleExecution(TestCycleExecutionDto testCycleExecutionDto) {
-        //TODO проверять на существование тест кейс
+        //TODO причесать
         User currentUser = securityUtils.getCurrentUser();
+        TestCycle testCycle = testCycleRepository.findById(testCycleExecutionDto.getTestCycleId()).orElseThrow(
+                () -> new EntityNotFoundException(TEST_CYCLE_NOT_FOUND_MESSAGE.formatted(testCycleExecutionDto.getTestCycleId())));
+        testCycle.setState(getTestCycleState(testCycleExecutionDto));
+        testCycleRepository.save(testCycle);
         testCycleExecutionDto.setUserId(currentUser.getId());
         testCycleExecutionDto.setExecutionDateTime(ZonedDateTime.now());
         TestCycleExecution testCycleExecution = testCycleExecutionRepository.save(testCycleExecutionMapper.testCycleExecutionDtoToTestCycleExecution(testCycleExecutionDto));
@@ -38,13 +47,30 @@ public class TestCycleExecutionService {
 
     public TestCycleExecutionDto getTestCycleExecution(Long id) {
         TestCycleExecution testCycleExecution = testCycleExecutionRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException(PROJECT_NOT_FOUND_MESSAGE.formatted(id)));
+                () -> new EntityNotFoundException(TEST_CYCLE_EXECUTION_NOT_FOUND_MESSAGE.formatted(id)));
         return testCycleExecutionMapper.testCycleExecutionToTestCycleExecutionDto(testCycleExecution);
     }
 
     public void deleteTestCycleExecution(Long id) {
         TestCycleExecution testCycleExecution = testCycleExecutionRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException(PROJECT_NOT_FOUND_MESSAGE.formatted(id)));
+                () -> new EntityNotFoundException(TEST_CYCLE_EXECUTION_NOT_FOUND_MESSAGE.formatted(id)));
         testCycleExecutionRepository.delete(testCycleExecution);
+    }
+
+    private TestCycleState getTestCycleState(TestCycleExecutionDto dto) {
+        if (dto.getTestsFail() != 0) {
+            return TestCycleState.FAILED;
+        }
+        if (dto.getTestsSkipped() != 0 || dto.getTestsBlocked() != 0) {
+            return TestCycleState.IN_PROGRESS;
+        }
+        if (dto.getTestsNotExecuted() != 0) {
+            if (dto.getTestsPassed() != 0) {
+                return TestCycleState.IN_PROGRESS;
+            } else {
+                return TestCycleState.NOT_EXECUTED;
+            }
+        }
+        return TestCycleState.PASSED;
     }
 }
